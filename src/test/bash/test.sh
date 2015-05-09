64dff5decf73c2e191cd710d690d30920975023e
Ovidiu Feodorov <ovidiu@novaordis.com>
2015-05-09 00:12:36 -0700

    basic overlay works

--- src/test/bash/test.sh
+++ src/test/bash/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 lib=$(dirname $0)/../../main/bash/lib/std.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
-lib=$(dirname $0)/../../main/bash/lib/ec2.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
+lib=$(dirname $0)/../../main/bash/lib/linux.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 
 verbose=true
-to-ids $@
+
+get-nfs-server-name ./fstab ./hosts
