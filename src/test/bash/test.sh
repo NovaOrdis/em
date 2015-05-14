ecefd56e87310f99f0d55b9265f08931062fd7cc
Ovidiu Feodorov <ovidiu@novaordis.com>
2015-05-13 17:59:20 -0700

    moved ./lib to ./bin/lib to keep the relative position of the libraries to executables consistent across the project

--- src/test/bash/test.sh
+++ src/test/bash/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-lib=$(dirname $0)/../../main/bash/lib/std.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
-lib=$(dirname $0)/../../main/bash/lib/linux.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
+lib=$(dirname $0)/../../main/bash/bin/lib/std.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
+lib=$(dirname $0)/../../main/bash/bin/lib/linux.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 
 verbose=true
 
