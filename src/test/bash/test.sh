948d79267bcd6f6ae742f054dc5df683596a00d9
Ovidiu Feodorov <ovidiu@novaordis.com>
2015-05-15 16:26:54 -0700

    AMQ broker installation: added functionality that starts the broker at boot

--- src/test/bash/test.sh
+++ src/test/bash/test.sh
@@ -2,7 +2,10 @@
 
 lib=$(dirname $0)/../../main/bash/bin/lib/std.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 lib=$(dirname $0)/../../main/bash/bin/lib/linux.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
+lib=$(dirname $0)/../../main/bash/bin/overlays/lib/amq-broker.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 
 verbose=true
 
-get-nfs-server-name ./fstab ./hosts
+configure-amq-to-start-at-boot ./activemq ./activemq.service
+
+
