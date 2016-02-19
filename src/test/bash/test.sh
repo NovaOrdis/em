ad7a581f273ba056ca03e7d6805702c8dd05ae86
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-02-19 00:14:07 -0800

    EAP subordinate host controller creation. Step 5: em handles subordinate host controller topology

--- src/test/bash/test.sh
+++ src/test/bash/test.sh
@@ -3,9 +3,10 @@
 lib=$(dirname $0)/../../main/bash/bin/lib/std.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 lib=$(dirname $0)/../../main/bash/bin/lib/linux.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 lib=$(dirname $0)/../../main/bash/bin/lib/ec2.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
+lib=$(dirname $0)/../../main/bash/bin/overlays/lib/eap.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 
-verbose=true
+#verbose=true
 
 #update-nfs-server-ip-in-local-config $@
-get-internal-ip-for-name $@
-
+#get-server-groups-from-topology-string $@
+last-line-containing $@
