0435f40549bbaf766447db93c2fde2c4018f7165
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-02-19 23:12:16 -0800

    unification of xml list handling. Step 1

--- src/test/bash/test.sh
+++ src/test/bash/test.sh
@@ -5,8 +5,8 @@ lib=$(dirname $0)/../../main/bash/bin/lib/linux.shlib; [ -f ${lib} ] && . ${lib}
 lib=$(dirname $0)/../../main/bash/bin/lib/ec2.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 lib=$(dirname $0)/../../main/bash/bin/overlays/lib/eap.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 
-#verbose=true
+verbose=true
 
-#update-nfs-server-ip-in-local-config $@
-#get-server-groups-from-topology-string $@
-last-line-containing $@
+out=$(get-element-names-corresponding-to-attribute "XXX" "a:AAA,b:XXX,c:CCC,d:XXX,e:MMM,f:XXX,b:XXX")
+echo ">${out}<"
+
