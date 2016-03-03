bf22f89d6d46cdaa86b80a6c9c39edea6838f375
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-03-03 15:47:15 -0800

    eap->jboss overlay, support for standalone, wildfly. Step 1

--- src/test/bash/test.sh
+++ src/test/bash/test.sh
@@ -3,7 +3,7 @@
 lib=$(dirname $0)/../../main/bash/bin/lib/std.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 lib=$(dirname $0)/../../main/bash/bin/lib/linux.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 lib=$(dirname $0)/../../main/bash/bin/lib/ec2.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
-lib=$(dirname $0)/../../main/bash/bin/overlays/lib/eap.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
+lib=$(dirname $0)/../../main/bash/bin/overlays/lib/jboss.shlib; [ -f ${lib} ] && . ${lib} || { echo "${lib} not found" 1>&2; exit 1; }
 
 verbose=true
 
